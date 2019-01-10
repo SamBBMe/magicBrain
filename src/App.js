c317b968f208c28f700918ca339f866c933d4b6d
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-01-09 23:18:59 -0500

    Added face recognition and face boxes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+//import SignIn from './components/SignIn/SignIn';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -29,7 +30,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
+      input: 'https://s17-us2.startpage.com/cgi-bin/serveimage?url=https:%2F%2Fsteamuserimages-a.akamaihd.net%2Fugc%2F478895272571535117%2F507261AA2503E9BEEB71D35BD43BA88320B1AD78%2F%3Finterpolation%3Dlanczos-none%26amp;output-format%3Djpeg%26amp;output-quality%3D95%26amp;fit%3Dinside%257C637%253A358%26amp;composite-to%3D*,*%257C637%253A358%26amp;background-color%3Dblack&sp=e2dc7e026b27f2d0d957fc6f0dd0d845',
       imageUrl: '',
       box: {}
     }
@@ -41,10 +42,10 @@ class App extends Component {
     const width = Number(image.width);
     const height = Number(image.height);
     return {
-      leftCol: clarifaiFace.lef_col * width,
-      toprow: clarifairFace.top_row * height,
-      rightCol: width - (clarifaiFace.rightCol * width),
-      bottomRow: height - (clarifairFace.bottom_row * height);
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
 
@@ -68,7 +69,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" onLoad={this.onButtonSubmit}>
         <Particles 
           className='particles' 
           params={particlesOptions}/>
@@ -79,7 +80,8 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
         <FaceRecognition
-          imageUrl = {this.state.imageUrl}/>
+          imageUrl = {this.state.imageUrl}
+          box = {this.state.box}/>
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) =>
+const FaceRecognition = ({ imageUrl, box }) =>
 {
     return(
         <div className='center ma'>
             <div className='absolute mt4'>
                 <img id='inputImage' alt='inputImg' src={imageUrl} width='500px' height='auto'/>
+                <div className='bounding-box' 
+                     style={{
+                            top: box.topRow,
+                            right: box.rightCol, 
+                            bottom: box.bottomRow, 
+                            left: box.leftCol}}>
+                </div>
             </div>
         </div>
     );
